a6ad48bafedd14f03b3484d9d0234948dedc5755
rogelioco <183464@ids.upchiapas.edu.mx>
2020-02-10 21:19:33 -0600

    Meta

--- Login/serializer.py
+++ Login/serializer.py
@@ -2,6 +2,6 @@ from rest_framework import routers, serializers, viewsets
 from Login.models import Example2
 
 class Example2Serializers(serializers.ModelSerializer):
-    class Metaa:
+    class Meta:
         model = Example2
         fields = ('__all__')
